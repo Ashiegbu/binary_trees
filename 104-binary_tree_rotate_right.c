c3e98f92842e97e73e1159864a20b4253ffd2086
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:03:36 -0800

    Rotate right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,27 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-  binary_tree_t *pivot, *tmp;
+binary_tree_t *pivot, *tmp;
 
-  if (tree == NULL || tree->left == NULL)
-    return (NULL);
+if (tree == NULL || tree->left == NULL)
+return (NULL);
 
-  pivot = tree->left;
-  tmp = pivot->right;
-  pivot->right = tree;
-  tree->left = tmp;
-  if (tmp != NULL)
-    tmp->parent = tree;
-  tmp = tree->parent;
-  tree->parent = pivot;
-  pivot->parent = tmp;
-  if (tmp != NULL)
-    {
-      if (tmp->left == tree)
-	tmp->left = pivot;
-      else
-	tmp->right = pivot;
-    }
+pivot = tree->left;
+tmp = pivot->right;
+pivot->right = tree;
+tree->left = tmp;
+if (tmp != NULL)
+tmp->parent = tree;
+tmp = tree->parent;
+tree->parent = pivot;
+pivot->parent = tmp;
+if (tmp != NULL)
+{
+if (tmp->left == tree)
+tmp->left = pivot;
+else
+tmp->right = pivot;
+}
 
-  return (pivot);
+return (pivot);
 }
