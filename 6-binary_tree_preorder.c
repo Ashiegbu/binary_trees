d8f8f3d0a87f77085e275b0bfc1925f52c31082c
Ashiegbu <chibuch@gmail.com>
2023-02-27 22:52:08 -0800

    Pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,10 +7,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-  if (tree && func)
-    {
-      func(tree->n);
-      binary_tree_preorder(tree->left, func);
-      binary_tree_preorder(tree->right, func);
-    }
+if (tree && func)
+{
+func(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
+}
 }
