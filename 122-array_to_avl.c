536f2794f561cbca04afb40b96170c6c5338356a
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:47:39 -0800

    AVL - Array to AVL

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -9,25 +9,25 @@
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-  avl_t *tree = NULL;
-  size_t i, j;
+avl_t *tree = NULL;
+size_t i, j;
 
-  if (array == NULL)
-    return (NULL);
+if (array == NULL)
+return (NULL);
 
-  for (i = 0; i < size; i++)
-    {
-      for (j = 0; j < i; j++)
-	{
-	  if (array[j] == array[i])
-	    break;
-	}
-      if (j == i)
-	{
-	  if (avl_insert(&tree, array[i]) == NULL)
-	    return (NULL);
-	}
-    }
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < i; j++)
+{
+if (array[j] == array[i])
+break;
+}
+if (j == i)
+{
+if (avl_insert(&tree, array[i]) == NULL)
+return (NULL);
+}
+}
 
-  return (tree);
+return (tree);
 }
