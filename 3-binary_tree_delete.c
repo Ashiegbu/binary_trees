5879d249859d7244bb545a71439a407e0391d104
Ashiegbu <chibuch@gmail.com>
2023-02-27 22:44:14 -0800

    Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,10 +6,10 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-  if (tree != NULL)
-    {
-      binary_tree_delete(tree->left);
-      binary_tree_delete(tree->right);
-      free(tree);
-    }
+if (tree != NULL)
+{
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
+free(tree);
+}
 }
