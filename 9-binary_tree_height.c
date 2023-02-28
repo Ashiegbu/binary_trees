e4d1b0d2f75ac25d4364edda2ab049960adea67a
Ashiegbu <chibuch@gmail.com>
2023-02-27 22:59:17 -0800

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  if (tree)
-    {
-      size_t l = 0, r = 0;
+if (tree)
+{
+size_t l = 0, r = 0;
 
-      l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-      r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-      return ((l > r) ? l : r);
-    }
-  return (0);
+l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+return ((l > r) ? l : r);
+}
+return (0);
 }
