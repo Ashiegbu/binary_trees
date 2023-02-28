ff9249e43cd3c4775e272dad43221b5b14e5fdd5
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:15:49 -0800

    BST - Array to BST

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -9,25 +9,25 @@
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-  bst_t *tree = NULL;
-  size_t i, j;
+bst_t *tree = NULL;
+size_t i, j;
 
-  if (array == NULL)
-    return (NULL);
+if (array == NULL)
+return (NULL);
 
-  for (i = 0; i < size; i++)
-    {
-      for (j = 0; j < i; j++)
-	{
-	  if (array[j] == array[i])
-	    break;
-	}
-      if (j == i)
-	{
-	  if (bst_insert(&tree, array[i]) == NULL)
-	    return (NULL);
-	}
-    }
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < i; j++)
+{
+if (array[j] == array[i])
+break;
+}
+if (j == i)
+{
+if (bst_insert(&tree, array[i]) == NULL)
+return (NULL);
+}
+}
 
-  return (tree);
+return (tree);
 }
