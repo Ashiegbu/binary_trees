0c185a64960d7e20e1b51dc404e57aa49945a8de
Ashiegbu <chibuch@gmail.com>
2023-02-27 23:03:19 -0800

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,5 +8,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
