99771e5c400810718fcb486cb85031a644b39077
Ashiegbu <chibuch@gmail.com>
2023-02-27 23:13:11 -0800

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-  size_t nodes = 0;
+size_t nodes = 0;
 
-  if (tree)
-    {
-      nodes += (tree->left || tree->right) ? 1 : 0;
-      nodes += binary_tree_nodes(tree->left);
-      nodes += binary_tree_nodes(tree->right);
-    }
-  return (nodes);
+if (tree)
+{
+nodes += (tree->left || tree->right) ? 1 : 0;
+nodes += binary_tree_nodes(tree->left);
+nodes += binary_tree_nodes(tree->right);
+}
+return (nodes);
 }
