6cd852fe92c0d70fff37e393100266ffc01c81dc
Ashiegbu <chibuch@gmail.com>
2023-02-28 01:01:57 -0800

    AVL - From sorted array

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -11,26 +11,26 @@
  */
 void create_tree(avl_t **node, int *array, size_t size, int mode)
 {
-  size_t middle;
+size_t middle;
 
-  if (size == 0)
-    return;
+if (size == 0)
+return;
 
-  middle = (size / 2);
-  middle = (size % 2 == 0) ? middle - 1 : middle;
+middle = (size / 2);
+middle = (size % 2 == 0) ? middle - 1 : middle;
 
-  if (mode == 1)
-    {
-      (*node)->left = binary_tree_node(*node, array[middle]);
-      create_tree(&((*node)->left), array, middle, 1);
-      create_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
-    }
-  else
-    {
-      (*node)->right = binary_tree_node(*node, array[middle]);
-      create_tree(&((*node)->right), array, middle, 1);
-      create_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
-    }
+if (mode == 1)
+{
+(*node)->left = binary_tree_node(*node, array[middle]);
+create_tree(&((*node)->left), array, middle, 1);
+create_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
+}
+else
+{
+(*node)->right = binary_tree_node(*node, array[middle]);
+create_tree(&((*node)->right), array, middle, 1);
+create_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
+}
 }
 
 /**
@@ -42,22 +42,22 @@ void create_tree(avl_t **node, int *array, size_t size, int mode)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-  avl_t *root;
-  size_t middle;
+avl_t *root;
+size_t middle;
 
-  root = NULL;
+root = NULL;
 
-  if (size == 0)
-    return (NULL);
+if (size == 0)
+return (NULL);
 
-  middle = (size / 2);
+middle = (size / 2);
 
-  middle = (size % 2 == 0) ? middle - 1 : middle;
+middle = (size % 2 == 0) ? middle - 1 : middle;
 
-  root = binary_tree_node(root, array[middle]);
+root = binary_tree_node(root, array[middle]);
 
-  create_tree(&root, array, middle, 1);
-  create_tree(&root, array + middle + 1, (size - 1 - middle), 2);
+create_tree(&root, array, middle, 1);
+create_tree(&root, array + middle + 1, (size - 1 - middle), 2);
 
-  return (root);
+return (root);
 }
