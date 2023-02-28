37d4797cfabebd9b9d74e0fe63e85ed718fadc38
Ashiegbu <chibuch@gmail.com>
2023-02-28 01:07:25 -0800

    Is Binary heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -7,25 +7,25 @@
  **/
 int check_max(const binary_tree_t *tree)
 {
-  int tmp1 = 1, tmp2 = 1;
+int tmp1 = 1, tmp2 = 1;
 
-  if (!tree)
-    return (0);
-  if (!tree->left && !tree->right)
-    return (1);
-  if (tree->left)
-    {
-      if (tree->n <= tree->left->n)
-	return (0);
-      tmp1 = check_max(tree->left);
-    }
-  if (tree->right)
-    {
-      if (tree->n <= tree->right->n)
-	return (0);
-      tmp2 = check_max(tree->right);
-    }
-  return (tmp1 && tmp2);
+if (!tree)
+return (0);
+if (!tree->left && !tree->right)
+return (1);
+if (tree->left)
+{
+if (tree->n <= tree->left->n)
+return (0);
+tmp1 = check_max(tree->left);
+}
+if (tree->right)
+{
+if (tree->n <= tree->right->n)
+return (0);
+tmp2 = check_max(tree->right);
+}
+return (tmp1 && tmp2);
 }
 /**
  * binary_tree_is_heap - checks if a binary tree is heap
@@ -34,13 +34,13 @@ int check_max(const binary_tree_t *tree)
  **/
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-  int tmp;
+int tmp;
 
-  if (!tree)
-    return (0);
+if (!tree)
+return (0);
 
-  tmp = binary_tree_is_complete(tree);
-  if (!tmp)
-    return (0);
-  return (check_max(tree));
+tmp = binary_tree_is_complete(tree);
+if (!tmp)
+return (0);
+return (check_max(tree));
 }
