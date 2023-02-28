15e0cf3526e5b3c79e137d01edd26be084fd81ce
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:07:03 -0800

    Is BST

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -11,14 +11,14 @@
  */
 int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 {
-  if (tree != NULL)
-    {
-      if (tree->n < lo || tree->n > hi)
-	return (0);
-      return (is_bst_helper(tree->left, lo, tree->n - 1) &&
-	      is_bst_helper(tree->right, tree->n + 1, hi));
-    }
-  return (1);
+if (tree != NULL)
+{
+if (tree->n < lo || tree->n > hi)
+return (0);
+return (is_bst_helper(tree->left, lo, tree->n - 1) &&
+is_bst_helper(tree->right, tree->n + 1, hi));
+}
+return (1);
 }
 
 /**
@@ -29,7 +29,7 @@ int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  return (is_bst_helper(tree, INT_MIN, INT_MAX));
+if (tree == NULL)
+return (0);
+return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
