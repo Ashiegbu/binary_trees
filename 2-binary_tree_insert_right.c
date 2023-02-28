2295144a7ba738a85ce4b2f06df0932248aabf0b
Ashiegbu <chibuch@gmail.com>
2023-02-27 22:41:34 -0800

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,21 +15,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new;
+binary_tree_t *new;
 
-  if (parent == NULL)
-    return (NULL);
+if (parent == NULL)
+return (NULL);
 
-  new = binary_tree_node(parent, value);
-  if (new == NULL)
-    return (NULL);
+new = binary_tree_node(parent, value);
+if (new == NULL)
+return (NULL);
 
-  if (parent->right != NULL)
-    {
-      new->right = parent->right;
-      parent->right->parent = new;
-    }
-  parent->right = new;
+if (parent->right != NULL)
+{
+new->right = parent->right;
+parent->right->parent = new;
+}
+parent->right = new;
 
-  return (new);
+return (new);
 }
