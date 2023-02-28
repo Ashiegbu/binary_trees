11f294db2e89225610a8c0d5fe4e6b6d9a9344fc
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:36:01 -0800

    Is AVL

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -13,15 +13,15 @@ int binary_tree_is_avl(const binary_tree_t *tree);
  */
 size_t height(const binary_tree_t *tree)
 {
-  if (tree)
-    {
-      size_t l = 0, r = 0;
+if (tree)
+{
+size_t l = 0, r = 0;
 
-      l = tree->left ? 1 + height(tree->left) : 1;
-      r = tree->right ? 1 + height(tree->right) : 1;
-      return ((l > r) ? l : r);
-    }
-  return (0);
+l = tree->left ? 1 + height(tree->left) : 1;
+r = tree->right ? 1 + height(tree->right) : 1;
+return ((l > r) ? l : r);
+}
+return (0);
 }
 
 /**
@@ -34,21 +34,21 @@ size_t height(const binary_tree_t *tree)
  */
 int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
 {
-  size_t lhgt, rhgt, diff;
+size_t lhgt, rhgt, diff;
 
-  if (tree != NULL)
-    {
-      if (tree->n < lo || tree->n > hi)
-	return (0);
-      lhgt = height(tree->left);
-      rhgt = height(tree->right);
-      diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
-      if (diff > 1)
-	return (0);
-      return (is_avl_helper(tree->left, lo, tree->n - 1) &&
-	      is_avl_helper(tree->right, tree->n + 1, hi));
-    }
-  return (1);
+if (tree != NULL)
+{
+if (tree->n < lo || tree->n > hi)
+return (0);
+lhgt = height(tree->left);
+rhgt = height(tree->right);
+diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
+if (diff > 1)
+return (0);
+return (is_avl_helper(tree->left, lo, tree->n - 1) &&
+is_avl_helper(tree->right, tree->n + 1, hi));
+}
+return (1);
 }
 
 /**
@@ -59,7 +59,7 @@ int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  return (is_avl_helper(tree, INT_MIN, INT_MAX));
+if (tree == NULL)
+return (0);
+return (is_avl_helper(tree, INT_MIN, INT_MAX));
 }
