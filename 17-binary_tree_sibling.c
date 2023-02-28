0f61608ca09996840f5e277737155bec4d9cea46
Ashiegbu <chibuch@gmail.com>
2023-02-27 23:28:54 -0800

    Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,9 +10,9 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-  if (node == NULL || node->parent == NULL)
-    return (NULL);
-  if (node->parent->left == node)
-    return (node->parent->right);
-  return (node->parent->left);
+if (node == NULL || node->parent == NULL)
+return (NULL);
+if (node->parent->left == node)
+return (node->parent->right);
+return (node->parent->left);
 }
