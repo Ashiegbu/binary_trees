aaff402af186a0d5b028582a9c06873da87dc065
Ashiegbu <chibuch@gmail.com>
2023-02-28 01:27:32 -0800

    Heap - Sort

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -8,19 +8,19 @@
  */
 size_t tree_size(const binary_tree_t *tree)
 {
-  size_t height_l = 0;
-  size_t height_r = 0;
+size_t height_l = 0;
+size_t height_r = 0;
 
-  if (!tree)
-    return (0);
+if (!tree)
+return (0);
 
-  if (tree->left)
-    height_l = 1 + tree_size(tree->left);
+if (tree->left)
+height_l = 1 + tree_size(tree->left);
 
-  if (tree->right)
-    height_r = 1 + tree_size(tree->right);
+if (tree->right)
+height_r = 1 + tree_size(tree->right);
 
-  return (height_l + height_r);
+return (height_l + height_r);
 }
 
 /**
@@ -34,20 +34,20 @@ size_t tree_size(const binary_tree_t *tree)
  **/
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-  int i, *a = NULL;
+int i, *a = NULL;
 
-  if (!heap || !size)
-    return (NULL);
+if (!heap || !size)
+return (NULL);
 
-  *size = tree_size(heap) + 1;
+*size = tree_size(heap) + 1;
 
-  a = malloc(sizeof(int) * (*size));
+a = malloc(sizeof(int) * (*size));
 
-  if (!a)
-    return (NULL);
+if (!a)
+return (NULL);
 
-  for (i = 0; heap; i++)
-    a[i] = heap_extract(&heap);
+for (i = 0; heap; i++)
+a[i] = heap_extract(&heap);
 
-  return (a);
+return (a);
 }
