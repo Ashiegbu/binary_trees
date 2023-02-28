203ff87b8b421a00b51a34c6a3d597d14be54061
Ashiegbu <chibuch@gmail.com>
2023-02-27 23:18:48 -0800

    Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,15 +9,15 @@
  */
 int is_full_recursive(const binary_tree_t *tree)
 {
-  if (tree != NULL)
-    {
-      if ((tree->left != NULL && tree->right == NULL) ||
-	  (tree->left == NULL && tree->right != NULL) ||
-	  is_full_recursive(tree->left) == 0 ||
-	  is_full_recursive(tree->right) == 0)
-	return (0);
-    }
-  return (1);
+if (tree != NULL)
+{
+if ((tree->left != NULL && tree->right == NULL) ||
+(tree->left == NULL && tree->right != NULL) ||
+is_full_recursive(tree->left) == 0 ||
+is_full_recursive(tree->right) == 0)
+return (0);
+}
+return (1);
 }
 
 /**
@@ -29,7 +29,7 @@ int is_full_recursive(const binary_tree_t *tree)
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  return (is_full_recursive(tree));
+if (tree == NULL)
+return (0);
+return (is_full_recursive(tree));
 }
