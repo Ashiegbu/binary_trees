9c03ba4df362e7f36aacf688a19a4b0ea9657a28
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:19:43 -0800

    BST - Search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -10,13 +10,13 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-  if (tree != NULL)
-    {
-      if (tree->n == value)
-	return ((bst_t *)tree);
-      if (tree->n > value)
-	return (bst_search(tree->left, value));
-      return (bst_search(tree->right, value));
-    }
-  return (NULL);
+if (tree != NULL)
+{
+if (tree->n == value)
+return ((bst_t *)tree);
+if (tree->n > value)
+return (bst_search(tree->left, value));
+return (bst_search(tree->right, value));
+}
+return (NULL);
 }
