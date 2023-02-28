2ca2f880a19393b82a3d803b69cfc823ed231393
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:12:13 -0800

    BST - Insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -9,40 +9,40 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-  bst_t *curr, *new;
+bst_t *curr, *new;
 
-  if (tree != NULL)
-    {
-      curr = *tree;
+if (tree != NULL)
+{
+curr = *tree;
 
-      if (curr == NULL)
-	{
-	  new = binary_tree_node(curr, value);
-	  if (new == NULL)
-	    return (NULL);
-	  return (*tree = new);
-	}
+if (curr == NULL)
+{
+new = binary_tree_node(curr, value);
+if (new == NULL)
+return (NULL);
+return (*tree = new);
+}
 
-      if (value < curr->n) /* insert in left subtree */
-	{
-	  if (curr->left != NULL)
-	    return (bst_insert(&curr->left, value));
+if (value < curr->n) /* insert in left subtree */
+{
+if (curr->left != NULL)
+return (bst_insert(&curr->left, value));
 
-	  new = binary_tree_node(curr, value);
-	  if (new == NULL)
-	    return (NULL);
-	  return (curr->left = new);
-	}
-      if (value > curr->n) /* insert in right subtree */
-	{
-	  if (curr->right != NULL)
-	    return (bst_insert(&curr->right, value));
+new = binary_tree_node(curr, value);
+if (new == NULL)
+return (NULL);
+return (curr->left = new);
+}
+if (value > curr->n) /* insert in right subtree */
+{
+if (curr->right != NULL)
+return (bst_insert(&curr->right, value));
 
-	  new = binary_tree_node(curr, value);
-	  if (new == NULL)
-	    return (NULL);
-	  return (curr->right = new);
-	}
-    }
-  return (NULL);
+new = binary_tree_node(curr, value);
+if (new == NULL)
+return (NULL);
+return (curr->right = new);
+}
+}
+return (NULL);
 }
