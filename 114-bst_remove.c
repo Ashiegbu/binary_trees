da9baff2bc72c52e59284e0db438427cb2b42728
Ashiegbu <chibuch@gmail.com>
2023-02-28 00:28:42 -0800

    BST - Remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -13,9 +13,9 @@ bst_t *bst_remove(bst_t *root, int value);
  */
 bst_t *inorder_successor(bst_t *root)
 {
-  while (root->left != NULL)
-    root = root->left;
-  return (root);
+while (root->left != NULL)
+root = root->left;
+return (root);
 }
 
 /**
@@ -27,39 +27,39 @@ bst_t *inorder_successor(bst_t *root)
  */
 bst_t *bst_delete(bst_t *root, bst_t *node)
 {
-  bst_t *parent = node->parent, *successor = NULL;
+bst_t *parent = node->parent, *successor = NULL;
 
-  /* No children or right-child only */
-  if (node->left == NULL)
-    {
-      if (parent != NULL && parent->left == node)
-	parent->left = node->right;
-      else if (parent != NULL)
-	parent->right = node->right;
-      if (node->right != NULL)
-	node->right->parent = parent;
-      free(node);
-      return (parent == NULL ? node->right : root);
-    }
+/* No children or right-child only */
+if (node->left == NULL)
+{
+if (parent != NULL && parent->left == node)
+parent->left = node->right;
+else if (parent != NULL)
+parent->right = node->right;
+if (node->right != NULL)
+node->right->parent = parent;
+free(node);
+return (parent == NULL ? node->right : root);
+}
 
-  /* Left-child only */
-  if (node->right == NULL)
-    {
-      if (parent != NULL && parent->left == node)
-	parent->left = node->left;
-      else if (parent != NULL)
-	parent->right = node->left;
-      if (node->left != NULL)
-	node->left->parent = parent;
-      free(node);
-      return (parent == NULL ? node->left : root);
-    }
+/* Left-child only */
+if (node->right == NULL)
+{
+if (parent != NULL && parent->left == node)
+parent->left = node->left;
+else if (parent != NULL)
+parent->right = node->left;
+if (node->left != NULL)
+node->left->parent = parent;
+free(node);
+return (parent == NULL ? node->left : root);
+}
 
-  /* Two children */
-  successor = inorder_successor(node->right);
-  node->n = successor->n;
+/* Two children */
+successor = inorder_successor(node->right);
+node->n = successor->n;
 
-  return (bst_delete(root, successor));
+return (bst_delete(root, successor));
 }
 
 /**
@@ -72,15 +72,15 @@ bst_t *bst_delete(bst_t *root, bst_t *node)
  */
 bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value)
 {
-  if (node != NULL)
-    {
-      if (node->n == value)
-	return (bst_delete(root, node));
-      if (node->n > value)
-	return (bst_remove_recursive(root, node->left, value));
-      return (bst_remove_recursive(root, node->right, value));
-    }
-  return (NULL);
+if (node != NULL)
+{
+if (node->n == value)
+return (bst_delete(root, node));
+if (node->n > value)
+return (bst_remove_recursive(root, node->left, value));
+return (bst_remove_recursive(root, node->right, value));
+}
+return (NULL);
 }
 
 /**
@@ -95,5 +95,5 @@ bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-  return (bst_remove_recursive(root, root, value));
+return (bst_remove_recursive(root, root, value));
 }
