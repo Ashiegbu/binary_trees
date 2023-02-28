c2ec16ad2fa132b9eb1a0fd7e1874e33ff208373
Ashiegbu <chibuch@gmail.com>
2023-02-27 23:35:39 -0800

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,17 +10,17 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
-  binary_tree_t *mom, *pop;
+binary_tree_t *mom, *pop;
 
-  if (!first || !second)
-    return (NULL);
-  if (first == second)
-    return ((binary_tree_t *)first);
+if (!first || !second)
+return (NULL);
+if (first == second)
+return ((binary_tree_t *)first);
 
-  mom = first->parent, pop = second->parent;
-  if (first == pop || !mom || (!mom->parent && pop))
-    return (binary_trees_ancestor(first, pop));
-  else if (mom == second || !pop || (!pop->parent && mom))
-    return (binary_trees_ancestor(mom, second));
-  return (binary_trees_ancestor(mom, pop));
+mom = first->parent, pop = second->parent;
+if (first == pop || !mom || (!mom->parent && pop))
+return (binary_trees_ancestor(first, pop));
+else if (mom == second || !pop || (!pop->parent && mom))
+return (binary_trees_ancestor(mom, second));
+return (binary_trees_ancestor(mom, pop));
 }
